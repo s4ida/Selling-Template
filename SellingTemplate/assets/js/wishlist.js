947b06522ab9d6b57d35380989f35afd025e55f8
Saida Shahalieva <146122051+s4ida@users.noreply.github.com>
2024-01-08 13:35:38 +0400

    Add files via upload

---
+++ SellingTemplate/assets/js/wishlist.js
@@ -0,0 +1,26 @@
+const wishlistdiv =document.getElementById('wishlistdiv')
+
+function getwishlist () {
+    wishlistdiv.innerHTML = ''
+
+    let wishlist = JSON.parse(localStorage.getItem('wishlist')) || []
+    wishlist.map((item,index) => {
+        let product = document.createElement('div')
+        product.className = 'wishBox col-12 col-sm-6 col-md-3 col-lg-3 col-xl-3'
+        product.innerHTML = `
+        <img src="${item.image}" alt="">
+        <p>"${item.name}"</p>
+        <p>"${item.price}"</p>
+        <button class="removefromwishlist" onclick="removefromwishlist(${index})">Remove from wishlist</button>
+        `
+        wishlistdiv.appendChild(product)
+    })
+}
+getwishlist();
+
+function removefromwishlist(index){
+    let wishlist = JSON.parse(localStorage.getItem('wishlist')) || []
+    wishlist.splice(index,1)
+    localStorage.setItem('wishlist',JSON.stringify(wishlist))
+    getwishlist() 
+}
